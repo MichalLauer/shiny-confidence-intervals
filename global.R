64d2e5eeb0b9978b9938c5f8f98e22a8582effeb
Michal Lauer <michal.lauer.25@gmail.com>
2023-07-02 11:44:17 +0200

    Reformatted libraries

--- global.R
+++ global.R
@@ -1,14 +1,17 @@
 # Load libraries
+# Shiny libs
 library(shiny)
 library(shinyjs)
 library(shinydashboard)
+# Shiny support
 library(distr6)
-library(stringr)
-library(dplyr)
 library(ggplot2)
-library(glue)
 library(DT)
 library(readr)
+# Data manipulation
+library(stringr)
+library(dplyr)
+library(glue)
 # Load functions
 sapply(X = list.files(path = "R/", full.names = T, recursive = T),
        FUN = source)
