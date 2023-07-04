b597aada781e25cb27bae92c1ddb0c9686189aa0
Michal Lauer <michal.lauer.25@gmail.com>
2023-07-04 09:08:30 +0200

    Deploying now using .R and secrets

---
+++ control/deploy.R
@@ -0,0 +1,4 @@
+rsconnect::setAccountInfo(name = 'laumi',
+                          token = Sys.getenv(SHINYAPPS_TOKEN),
+                          secret = Sys.getenv(SHINYAPPS_SECRET))
+rsconnect::deployApp(appDir = './')
